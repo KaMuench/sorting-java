08edbaeb41108215ea27969b8cc393a8cb13d887
KaMuench <muench.kaleb@gmail.com>
2024-06-25 18:04:01 +0200

    fixed test

--- src/test/java/com/muench/kaleb/sorting/algorithms/SortingTests.java
+++ src/test/java/com/muench/kaleb/sorting/algorithms/SortingTests.java
@@ -5,7 +5,7 @@ public class SortingTests {
 
     @Test
     public void testSort() {
-        BubbleSort<Integer> bubbleSort = new BubbleSort<>(null);
+        BubbleSort<Integer> bubbleSort = new BubbleSort<>();
         Integer[] list = {5, 4, 3, 2, 1};
         Integer[] sortedList = bubbleSort.sort(list);
         Assertions.assertArrayEquals(new Integer[]{1, 2, 3, 4, 5}, sortedList);
